ce2adf0c9e65a8800eb3c48640455a0f65a524a0
Knoageo <knoageo7@gmail.com>
2024-08-18 11:26:20 +0900

    2

---
+++ picturelib copy.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>picture</title>
+    <link rel="stylesheet" href="style/themestyle-sp.css">
+</head>
+<body>
+<header>組み込みphp</header>
+<footer>組み込みphp</footer>
+</body>
+</html>
