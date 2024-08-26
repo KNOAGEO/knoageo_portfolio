09969e23ca1a01bae598e9b85013428f65e58dcb
Knoageo <knoageo7@gmail.com>
2024-08-26 21:35:25 +0900

    2

--- header.php
+++ header.php
@@ -9,27 +9,32 @@
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style/themestyle-sp.css" />
     <!--font awesome-->
     <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/style/all.min.css">
-    <link rel="preconnect" href="<?php echo get_template_directory_uri(); ?>/https://fonts.googleapis.com">
-    <link rel="preconnect" href="<?php echo get_template_directory_uri(); ?>/https://fonts.gstatic.com" crossorigin />
-    <link href="<?php echo get_template_directory_uri(); ?>/https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique&display=swap" rel="stylesheet" />
-    <link rel="<?php echo get_template_directory_uri(); ?>/preconnect" href="https://fonts.googleapis.com">
-    <link rel="<?php echo get_template_directory_uri(); ?>/preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link href="<?php echo get_template_directory_uri(); ?>/https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet" />
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet" />
+    <!-- slick(カルーセル) -->
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="<?php echo get_template_directory_uri(); ?>/https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <script type="text/javascript" src="<?php echo get_template_directory_uri(); ?>/https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- リセットcss -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
     <?php wp_head(); ?>
-</head>
-
-<body>
- <div class="super_container">
+  </head>
+  <body>
 <!--ヘッダーここから-->
-  <header class="header">
-   <div class="logoarea">
-    <a href="/">
-     <img src="<?php echo get_template_directory_uri(); ?>/image/KNOAGEOロゴ1.png" alt="トップページに戻る" />
+  <header>
+    <div class="navarea">
+   <a href="top.php" id="logo"><div class="logoarea"><img src="image/KNOAGEOロゴ1.png" alt="トップページに戻る" /></div></a>
+
  <!-- スマホ用メニューボタン -->
 <div class="hamburger-menu">
   <a href="#"><img src="image/白樺.png" alt="ナビゲーションメニューを開く" onclick="document.getElementById('nav-sp').style.display = 'block'" ></a>
 </div>
-
+    </div>
 <!-- PC用ナビ--><!--
 <nav id="nav-pc">
  <div class="search-box">
@@ -49,37 +54,49 @@
 <nav id="nav-sp">
   <h1 class="title">メニュー</h1>
   <hr class="light-mainbar" />
-  <img class="close" src="image/白樺×ボタン.png" alt="画面を閉じる" onclick="document.getElementById('nav-sp').style.display = 'none'">
+  <div class="close">
+  <img src="image/白樺×ボタン.png" alt="画面を閉じる" onclick="document.getElementById('nav-sp').style.display = 'none'">
+  </div>
   <div class="search-box">
-    <img src="image/検索アイコン.png">
     <input type="text" id="search-query" placeholder="検索ワードを入力してください...">
-      <button onclick="performSearch()">検索</button>
+    <div class="search-icon">
+    <img src="image/検索アイコン.png" alt="検索" onclick="performSearch()">
       </div>
       <!-- 結果表示エリア -->
       <div class="results" id="search-results">
         <!-- 検索結果がここに表示されます -->
       </div>
-  <span>カテゴリー</span>
-   <a class="menu" href="picture.php" onclick="document.getElementById('nav-sp').style.display = 'none'">写真</a>
-   <a href="illust.php" onclick="document.getElementById('nav-sp').style.display = 'none'">イラスト</a>
-   <a href="weblog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">Webサイト作例</a>
-   <span>その他関連</span>
-   <a href="blog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">ブログ</a>
-   <a href="originalsite.php" onclick="document.getElementById('nav-sp').style.display = 'none'">制作サイト</a>
-   <span>自己紹介</span>
-   <a href="profile.php" onclick="document.getElementById('nav-sp').style.display = 'none'">自己紹介をよりくわしく</a>
-   <span>扱うスキル一覧</span>
-   <span>作例ライブラリ</span>
-   <a href="picturelib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">写真</a>
-   <a href="picturelib.php#teiten" onclick="document.getElementById('nav-sp').style.display = 'none'">TEITEN（定点観測）</a>
-   <a href="illustlib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">イラスト</a>
-   <a href="webloglib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">Webサイト</a>
-   <a href="blog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">ブログ</a>
-   <a href="originalsite.php" onclick="document.getElementById('nav-sp').style.display = 'none'">制作サイト</a>
-   <span>お問い合わせ</span>
-   <a href="contact.php" onclick="document.getElementById('nav-sp').style.display = 'none'">お問い合わせフォームへ</a>
+    </div>
+  <div class="category">カテゴリー</div>
+    <a class="menu" href="picture.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊写真</a>
+    <a class="menu" href="illust.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊イラスト</a>
+    <a class="menu" href="weblog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊Webサイト作例</a>
+   <div class="category">その他関連</div>
+   <a class="menu" href="blog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊ブログ</a>
+   <a class="menu" href="originalsite.php" onclick="document.getElementById('nav-sp').style.display='none'">＊制作サイト</a>
+   <div class="category">自己紹介</div>
+   <a class="menu" href="profile.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊自己紹介をよりくわしく</a>
+   <div class="category">扱うスキル一覧</div>
+   <!-- プルボタン -->
+   <i class="fas fa-sort-down"></i>
+   <div>
+     <!-- photoshop -->
+    <!-- Illustrator -->
+     <!-- ibispaint -->
+      <img src="image/vscode.png">
+      <img src="image/docker.png">
+      <img src="image/github.png">
+      <img src="image/Figma.png">
+   </div>
+   <div class="category">作例ライブラリ</div>
+   <a class="menu" href="picturelib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊写真</a>
+   <a class="menu" href="picturelib.php#teiten" onclick="document.getElementById('nav-sp').style.display = 'none'">∟TEITEN（定点観測）</a>
+   <a class="menu" href="illustlib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊イラスト</a>
+   <a class="menu" href="webloglib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊Webサイト</a>
+   <a class="menu" href="blog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊ブログ</a>
+   <a class="menu" href="originalsite.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊制作サイト</a>
+   <div class="category">お問い合わせ</div>
+   <a class="menu" href="contact.php" onclick="document.getElementById('nav-sp').style.display = 'none'">∟お問い合わせフォームへ</a>
  </nav>
-</div>
 <hr class="light-topbar" />
 </header>
-<!--ヘッダー終わり-->
--- top.php
+++ top.php
@@ -1,6 +1,292 @@
-<?php get_header();?>
-<div class="home">
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <title>KNOAGEO</title>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="description" content="knoageo portfolio">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="style/themestyle-sp.css" />
+    <!--font awesome-->
+    <link rel="stylesheet" type="text/css" href="style/all.min.css">
+    <!-- font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Zen+Kaku+Gothic+Antique&display=swap" rel="stylesheet" />
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+    <link href="https://fonts.googleapis.com/css2?family=Lexend+Exa:wght@100..900&display=swap" rel="stylesheet" />
+    <!-- slick(カルーセル) -->
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick-theme.css">
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <!-- リセットcss -->
+    <link
+    rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/destyle.css@1.0.15/destyle.css"/>
+  </head>
+  <body>
+<!--ヘッダーここから-->
+  <header>
+    <div class="navarea">
+   <a href="top.php" id="logo"><div class="logoarea"><img src="image/KNOAGEOロゴ1.png" alt="トップページに戻る" /></div></a>
 
+ <!-- スマホ用メニューボタン -->
+<div class="hamburger-menu">
+  <a href="#"><img src="image/白樺.png" alt="ナビゲーションメニューを開く" onclick="document.getElementById('nav-sp').style.display = 'block'" ></a>
+</div>
+    </div>
+<!-- PC用ナビ--><!--
+<nav id="nav-pc">
+ <div class="search-box">
+  <i class="fa-solid fa-magnifying-glass"></i>
+  <input type="text" id="search-query" placeholder="検索ワードを入力してください...">
+    <button onclick="performSearch()">検索</button>
+    </div>-->
+    <!-- 結果表示エリア --><!--
+    <div class="results" id="search-results">-->
+      <!-- 検索結果がここに表示されます -->
+       <!--
+    </div>
+ <span>メニュー</span>
+</nav>-->
 
-</div>    
-<?php get_footer();?>
+<!--スマホ用ナビ -->
+<nav id="nav-sp">
+  <h1 class="title">メニュー</h1>
+  <hr class="light-mainbar" />
+  <div class="close">
+  <img src="image/白樺×ボタン.png" alt="画面を閉じる" onclick="document.getElementById('nav-sp').style.display = 'none'">
+  </div>
+  <div class="search-box">
+    <input type="text" id="search-query" placeholder="検索ワードを入力してください...">
+    <div class="search-icon">
+    <img src="image/検索アイコン.png" alt="検索" onclick="performSearch()">
+      </div>
+      <!-- 結果表示エリア -->
+      <div class="results" id="search-results">
+        <!-- 検索結果がここに表示されます -->
+      </div>
+    </div>
+  <div class="category">カテゴリー</div>
+    <a class="menu" href="picture.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊写真</a>
+    <a class="menu" href="illust.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊イラスト</a>
+    <a class="menu" href="weblog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊Webサイト作例</a>
+   <div class="category">その他関連</div>
+   <a class="menu" href="blog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊ブログ</a>
+   <a class="menu" href="originalsite.php" onclick="document.getElementById('nav-sp').style.display='none'">＊制作サイト</a>
+   <div class="category">自己紹介</div>
+   <a class="menu" href="profile.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊自己紹介をよりくわしく</a>
+   <div class="category">扱うスキル一覧</div>
+   <!-- プルボタン -->
+   <i class="fas fa-sort-down"></i>
+   <div>
+     <!-- photoshop -->
+    <!-- Illustrator -->
+     <!-- ibispaint -->
+      <img src="image/vscode.png">
+      <img src="image/docker.png">
+      <img src="image/github.png">
+      <img src="image/Figma.png">
+   </div>
+   <div class="category">作例ライブラリ</div>
+   <a class="menu" href="picturelib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊写真</a>
+   <a class="menu" href="picturelib.php#teiten" onclick="document.getElementById('nav-sp').style.display = 'none'">∟TEITEN（定点観測）</a>
+   <a class="menu" href="illustlib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊イラスト</a>
+   <a class="menu" href="webloglib.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊Webサイト</a>
+   <a class="menu" href="blog.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊ブログ</a>
+   <a class="menu" href="originalsite.php" onclick="document.getElementById('nav-sp').style.display = 'none'">＊制作サイト</a>
+   <div class="category">お問い合わせ</div>
+   <a class="menu" href="contact.php" onclick="document.getElementById('nav-sp').style.display = 'none'">∟お問い合わせフォームへ</a>
+ </nav>
+<hr class="light-topbar" />
+</header>
+<!-- ヘッダーおわり -->
+<!--メイン-->
+<main>
+  <!--カバー-->
+  <div class="cover">
+    <img src="./image/picture1.jpg" />
+  </div>
+  <div class="sec">
+  <section>
+    <div class="category-wrapper">
+      <div class="title-2">お知らせ</div>
+      <div class="descarea">
+      <div class="description">
+        <p>現在メンテナンス中です<br>(リサーチ機能・ブログ＆Webサイトページ実装中)</p>
+      </div>
+      <div class="date">2024.5.5</div>
+    </div>
+    </div>
+  </section>
+  <!-- カテゴリ枠 -->
+  <!--写真-->
+  <section class="picture-cor">
+    <hr class="light-mainbar" />
+    <div class="title-wrapper">
+    <p class="title">写真</p>
+    <p class="subtitle">Picture</p>
+    </div>
+    <img src="image/top-picture1.jpg">
+    <div class="category-wrapper">
+      <div class="title-2">
+        <p>春日山の朝景色</p>
+      </div>
+        <div class="subcomment">NikonD850 Nikkor24-70mm f2.8</div>
+          <div class="date">2024.5.5</div>
+        <div class="descarea">
+          <p>自然・伝統的な景色を主に撮っています<br>目覚め・静かな情熱を<br>感じさせる一瞬が好きです</p>
+          <p>使用機種<br>＊Nikon D850<br>＊Nikon D7000</p>
+        <a href="picturelib.html">
+        <button class="move">もっとみる➔</button></a>
+        </div>
+      </div>
+  </section>
+
+   <!--イラスト-->
+   <section class="illust-cor">
+    <hr class="light-mainbar" />
+    <div class="title-wrapper">
+    <p class="title">イラスト</p>
+      <p class="subtitle">Illustration</p>
+    </div>
+    <img src="image/comming soonイラスト.png">
+    <div class="category-wrapper">
+      <div class="title-2">
+        <p>にほんかもしか</p>
+      </div>
+        <div class="subcomment">にほんかもしか</div>
+        <div class="date">2024.5.5</div>
+          <div class="descarea">
+            <p>動物・キャラクター・想像画<br>デジタルイラスト中心で描いています</p>
+            <p>使用ソフト<br>＊ibisPaintX<br>＊Photoshop/Illustrator<br>＊CLIP STUDIO PAINT</p>
+          <a href="illustlib.html">
+          <button class="move">もっとみる➔</button></a>
+          </div>
+      </div>
+  </section>
+  <!--Webサイト作例-->
+  <section class="web-cor">
+    <hr class="light-mainbar" />
+    <div class="title-wrapper">
+    <p class="title">Webサイト</p>
+      <p class="subtitle">Website log</p>
+      </div>
+      <div class="slide">
+      <ul class="slider">
+        <li><img src="image/comming soonイラスト.png" alt="img01"></li>
+        <li><img src="image/comming soonイラスト.png" alt="img02"></li>
+        <li><img src="image/comming soonイラスト.png" alt="img03"></li>
+      </ul>
+    </div>
+    <div class="category-wrapper">
+      <p>得意なコンセプト：ナチュラル・シンプル・モダン</p>
+      <p>ご依頼をもとに制作した作品です</p>
+      <span style="color: rgba(255, 168, 109, 0.829);">※掲載に関しては依頼元の許可をいただいております</span>
+      <p>Webサイト制作は随時受け付けていますので<br>ご相談・ご依頼はお問い合わせフォームまでお願いいたします</p>
+      <div class="descarea">
+        <h2>使用用語・環境</h2>
+        <h3>OS:</h3>
+        <p>Windows/Mac(iOS)</p><h3>Server:</h3><p>Lunix</p>
+        <h3>言語:</h3>
+        <p>HTML・CSS・JavaScript・php・jQuery</p>
+        <h3>その他環境:</h3>
+        <p>GitHub・VScode・WordPress・Local flywheel・Docker・Figma・MySQL・Adobe Photoshop・Illustrator</p>
+        <a href="webloglib.html">
+        <button class="move">もっとみる➔</button></a>
+      </div>
+    </div>
+  </section>
+
+  <!--その他関連--※中身がまだないため実装はあと。カルーセルに沿って説明切替を実装
+  <section class="other-cor">
+    <hr class="light-mainbar" />
+    <div class="title-wrapper">
+    <p class="title">その他関連</p>
+      <p class="subtitle">Other</p>
+    </div>
+    <div class="slide">
+      <ul class="slider">
+        <li><img src="image/comming soonイラスト.png" alt="img01"></li>
+        <li><img src="image/comming soonイラスト.png" alt="img02"></li>
+        <li><img src="image/comming soonイラスト.png" alt="img03"></li>
+      </ul>
+    </div>
+    <div class="category-wrapper">
+      <div class="descarea">
+        <p>カルーセルに沿って説明</p>
+        <a href="library.php#blog">
+        <button class="move">もっとみる➔</button></a>
+      </div>
+    </div>
+  </section>-->
+
+  <!--自己紹介-->
+  <section class="profile-cor">
+    <hr class="light-mainbar" />
+    <div class="title-wrapper">
+    <p class="title">自己紹介</p>
+    <p class="subtitle">Profile</p>
+   </div>
+    <img src="image/myprofile3.jpg">
+    <div class="category-wrapper">
+      <div class="descarea">
+        <div class="title-wrapper">
+          <p class="title-1">森 真由美</p>
+          <p class="subtitle-1">Mayumi Mori</p>
+          </div>
+        <hr>
+        <div class="desclist-left">
+        <p>＊生まれ： 1996年</p>
+        <p>＊出身：佐賀県出身/愛知県在住</p>
+        <p>＊趣味：写真撮影・絵描き・旅行・スノーボード・国立公園/道の駅/温泉巡り</p>
+        <p>＊職業: soon</p>
+        <p>＊特記: 聴覚に障がいがあります</p>
+      </div>
+        <hr>
+        <!-- SNSアイコン -->
+         <div class="linkarea">
+        <div class="sns-area">
+       <div class="sns-icon-insta">
+      <a href="https://www.instagram.com/mayu_knoageo7?igsh=MTllM2FiZ2J6dHM4ag%3D%3D&utm_source=qr"><img src="image/Instagram.png" alt="instagramのリンク"></a>
+      </div>
+      <div class="sns-icon-threads">
+      <a href="https://www.threads.net/@mayu_knoageo7"><img src="image/threads.png" alt="Threadsのリンク"></a>
+      </div>
+    </div>
+    <div>
+      <a href="profile.html">
+      <button class="move">詳細はこちら➔</button></a>
+    </div>
+      </div> 
+    </div>
+  </div>
+  </section>
+ </div>
+</main>
+
+<footer>
+  <hr class="light-mainbar" />
+  <div class="footerzone">
+  <a href="#"><img src="image/KNOAGEOロゴ1.png" alt="トップページに戻る"></a>
+  </div>
+  <div class="ft-contactarea">
+    <p>e-mail: receptionmail@knoageo7.com</p>
+    <p>対応時間: 6:00-19:00<br><span>随時返信いたしますが、半日～1日お待ちいただく場合がございます</span></p>
+    
+  </div>
+  <div class="ft-centerarea">
+    <a href="contact.html"><p>お問い合わせ・ご依頼や相談はこちら</p></a>
+    <span style="color: rgba(255, 168, 109, 0.829);">本サイト内の画像・作例などコンテンツの無断利用を固く禁じます</span>
+    <a href="contact.html#content"><p>コンテンツ利用申請はこちら</p></a>
+  </div>
+  <div class="copyright">&copy;2025 KNOAGEO</div>
+</footer>
+<!-- jQueryをCDNで導入するためのコード -->
+<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>  
+<script type="text/javascript" src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<script src="js/top.js"></script>
+
+</body>
+</html>
