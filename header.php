9602902bc0a69d8d35254680c520f45de00ad176
Knoageo <knoageo7@gmail.com>
2024-09-08 01:27:03 +0900

    Header完了

--- header.php
+++ header.php
@@ -27,6 +27,7 @@
   <body>
 <!--ヘッダーここから-->
   <header>
+    <!-- ================================スマホ用メニューボタン================================-->
     <div class="navarea">
    <a href="/" id="logo">
   <div class="logoarea"><img src="<?php echo get_template_directory_uri(); ?>/image/KNOAGEOロゴ1.png" alt="トップページに戻る" /></div></a>
@@ -35,23 +36,8 @@
 <div class="hamburger-menu">
   <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/白樺.png" alt="ナビゲーションメニューを開く" onclick="document.getElementById('nav-sp').style.display = 'block'" ></a>
 </div>
-    </div>
-<!-- PC用ナビ--><!--
-<nav id="nav-pc">
- <div class="search-box">
-  <i class="fa-solid fa-magnifying-glass"></i>
-  <input type="text" id="search-query" placeholder="検索ワードを入力してください...">
-    <button onclick="performSearch()">検索</button>
-    </div>-->
-    <!-- 結果表示エリア --><!--
-    <div class="results" id="search-results">-->
-      <!-- 検索結果がここに表示されます -->
-       <!--
-    </div>
- <span>メニュー</span>
-</nav>-->
+</div>
 
-<!--スマホ用ナビ -->
 <nav id="nav-sp">
   <h1 class="title">メニュー</h1>
   <hr class="light-mainbar" />
@@ -83,9 +69,70 @@
     <a class="menu" href="<?php echo home_url(); ?>/webloglib" onclick="document.getElementById('nav-sp').style.display = 'none'">＊Webサイト</a>
     <a class="menu" href="<?php echo home_url(); ?>/blog" onclick="document.getElementById('nav-sp').style.display = 'none'">＊ブログ</a>
     <a class="menu" href="<?php echo home_url(); ?>/originalsite" onclick="document.getElementById('nav-sp').style.display = 'none'">＊制作サイト</a>
-
   <div class="category">お問い合わせ</div>
     <a class="menu" href="<?php echo home_url(); ?>/contact" onclick="document.getElementById('nav-sp').style.display = 'none'">∟お問い合わせフォームへ</a>
  </nav>
+
+ <!-- ================================PC用メニューボタン================================-->
+ <div class="navarea-pc">
+   <a href="/" id="logo">
+  <div class="logoarea"><img src="<?php echo get_template_directory_uri(); ?>/image/KNOAGEOロゴ1.png" alt="トップページに戻る" /></div></a>
+
+ <div class="menu-bar">
+    <div class="hamburger-menu-pc">
+      <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/検索アイコン.png" alt="検索メニューを開く" onclick="document.getElementById('nav-pc-search').style.display = 'block'" >検索</a>
+    </div>
+    <div class="hamburger-menu-pc">
+      <a href="#"><img src="<?php echo get_template_directory_uri(); ?>/image/白樺.png" alt="ナビゲーションメニューを開く" onclick="document.getElementById('nav-pc').style.display = 'block'" >メニュー</a>
+    </div>
+    </div>
+  </div>
+<!-- PC用検索-->
+<nav id="nav-pc-search">
+    <h1 class="title-2pc">検索</h1>
+    <hr class="light-mainbar" />
+    <div class="search-box">
+      <input type="text" id="search-query" placeholder="検索ワードを入力してください...">
+      <div class="search-icon">
+      <img src="<?php echo get_template_directory_uri(); ?>/image/検索アイコン.png" alt="検索" onclick="performSearch()">
+        </div>
+        <div class="close-1">
+    <img src="<?php echo get_template_directory_uri(); ?>/image/白樺×ボタン.png" alt="画面を閉じる" onclick="document.getElementById('nav-pc-search').style.display = 'none'">
+    </div>
+        <!-- 結果表示エリア -->
+        <div class="results" id="search-results">
+          <!-- 検索結果がここに表示されます -->
+        </div>
+      </div>
+</nav>
+</div> <!--  navarea閉じ  -->
+<!-- PC用menu-->
+<nav id="nav-pc">
+  <h1 class="title-2pc">メニュー</h1>
+    <div class="close">
+       <img src="<?php echo get_template_directory_uri(); ?>/image/白樺×ボタン.png" alt="画面を閉じる" onclick="document.getElementById('nav-pc').style.display = 'none'">
+    </div>
+    <hr class="light-mainbar" />
+  <div class="category">カテゴリー</div>
+  <a class="menu" href="<?php echo home_url(); ?>/picture" onclick="document.getElementById('nav-pc').style.display = 'none'">＊写真</a>
+  <a class="menu" href="<?php echo home_url(); ?>/illustration" onclick="document.getElementById('nav-pc').style.display = 'none'">＊イラスト</a>
+  <a class="menu" href="<?php echo home_url(); ?>/weblog" onclick="document.getElementById('nav-pc').style.display = 'none'">＊Webサイト作例</a>
+ <div class="category">その他関連</div>
+ <a class="menu" href="<?php echo home_url(); ?>/library/#blog" onclick="document.getElementById('nav-pc').style.display = 'none'">＊ブログ</a>
+ <a class="menu" href="<?php echo home_url(); ?>/library/#originalsite" onclick="document.getElementById('nav-pc').style.display='none'">＊制作サイト</a>
+ <div class="category">自己紹介</div>
+ <a class="menu" href="<?php echo home_url(); ?>/profile" onclick="document.getElementById('nav-pc').style.display = 'none'">＊自己紹介をよりくわしく</a>
+ <div class="category">作例ライブラリ<a href="<?php echo home_url(); ?>/library" onclick="document.getElementById('nav-sp').style.display = 'none'"></div>
+ <a class="menu" href="<?php echo home_url(); ?>/library/picturelib" onclick="document.getElementById('nav-pc').style.display = 'none'">＊写真</a>
+ <a class="menu" href="<?php echo home_url(); ?>/library/picturelib#teiten" onclick="document.getElementById('nav-pc').style.display = 'none'">∟TEITEN（定点観測）</a>
+ <a class="menu" href="<?php echo home_url(); ?>/illustlib" onclick="document.getElementById('nav-pc').style.display = 'none'">＊イラスト</a>
+ <a class="menu" href="<?php echo home_url(); ?>/webloglib" onclick="document.getElementById('nav-pc').style.display = 'none'">＊Webサイト</a>
+ <a class="menu" href="<?php echo home_url(); ?>/blog" onclick="document.getElementById('nav-pc').style.display = 'none'">＊ブログ</a>
+ <a class="menu" href="<?php echo home_url(); ?>/originalsite" onclick="document.getElementById('nav-pc').style.display = 'none'">＊制作サイト</a>
+ <div class="category">お問い合わせ</div>
+ <a class="menu" href="<?php echo home_url(); ?>/contact" onclick="document.getElementById('nav-pc').style.display = 'none'">∟お問い合わせフォームへ</a>
+</nav>
+</div> <!-- PC版　ナビゲーションメニュー閉じ※スマホ非表示ここまで--> 
+
  <hr class="light-topbar" />
 </header>
